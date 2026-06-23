7c7fdbdb4c3d0809d985a6f862a41dea71c13fd8
Earl John Gomez <earljohngomez66@gmail.com>
2026-06-23 15:18:19 +0800

    add section ids for nav anchor scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,13 @@ export default function Home() {
       <Navigation />
       <div className="max-w-6xl mx-auto px-4 pt-20 pb-10 grid lg:grid-cols-3 gap-6">
         <aside className="space-y-5">
-          <AnimatedCard delay={0} className="p-5"><Hero /></AnimatedCard>
-          <AnimatedCard delay={0.1} className="p-5"><Contact /></AnimatedCard>
+          <AnimatedCard delay={0} className="p-5"><div id="home"><Hero /></div></AnimatedCard>
+          <AnimatedCard delay={0.1} className="p-5"><div id="contact"><Contact /></div></AnimatedCard>
           <AnimatedCard delay={0.2} className="p-5"><Interests /></AnimatedCard>
           <AnimatedCard delay={0.3} className="p-5"><TechStack /></AnimatedCard>
           <AnimatedCard delay={0.4} className="p-5"><SocialLinks /></AnimatedCard>
         </aside>
-        <div className="lg:col-span-2 space-y-5">
+        <div id="about" className="lg:col-span-2 space-y-5">
           <AnimatedCard delay={0} className="p-5"><Experience /></AnimatedCard>
           <AnimatedCard delay={0.1} className="p-5"><Education /></AnimatedCard>
           <AnimatedCard delay={0.2} className="p-5"><Projects /></AnimatedCard>
